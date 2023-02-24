3c2ea61756908c8d1e5a27a32b4045566d8d45ee
Javier Eusebio <javiergeusebio@gmail.com>
2023-02-24 09:49:59 -0300

    Corrección  error menu se abre de cualquier parte + Readme

--- menu.js
+++ menu.js
@@ -8,13 +8,13 @@ document.getElementById('header').classList.toggle('show')
 
 
 document.getElementById('nav').addEventListener('click',function(){
-  document.getElementById('button-menu').classList.toggle('close')
-  document.getElementById('nav').classList.toggle('show')
-  document.getElementById('header').classList.toggle('show')
+  document.getElementById('button-menu').classList.remove('close')
+  document.getElementById('nav').classList.remove('show')
+  document.getElementById('header').classList.remove('show')
   });
 
   document.getElementById('content').addEventListener('click',function(){
-    document.getElementById('button-menu').classList.toggle('close')
-    document.getElementById('nav').classList.toggle('show')
-    document.getElementById('header').classList.toggle('show')
+    document.getElementById('button-menu').classList.remove('close')
+    document.getElementById('nav').classList.remove('show')
+    document.getElementById('header').classList.remove('show')
     });
