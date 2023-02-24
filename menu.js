e0693f4b351311dc58a47dd15459e6604c4ba0d8
Javier <javiergeusebio@gmail.com>
2023-02-24 02:57:10 -0300

    Correcciones al menu.js para mejorar funcion

--- menu.js
+++ menu.js
@@ -4,13 +4,17 @@ document.getElementById('button-menu').addEventListener('click',function(){
 document.getElementById('button-menu').classList.toggle('close')
 document.getElementById('nav').classList.toggle('show')
 document.getElementById('header').classList.toggle('show')
-
 });
 
 
-document.getElementById('nav').addEventListener('click',e => {
-  if(e.target.id === 'nav'){
-    document.getElementById('nav').classList.remove('show')
-    document.getElementById('button-menu').classList.remove('close')
-  }
-  })
+document.getElementById('nav').addEventListener('click',function(){
+  document.getElementById('button-menu').classList.toggle('close')
+  document.getElementById('nav').classList.toggle('show')
+  document.getElementById('header').classList.toggle('show')
+  });
+
+  document.getElementById('content').addEventListener('click',function(){
+    document.getElementById('button-menu').classList.toggle('close')
+    document.getElementById('nav').classList.toggle('show')
+    document.getElementById('header').classList.toggle('show')
+    });
